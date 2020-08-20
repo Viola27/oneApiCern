ba54867cf3f6c5abaea2ec802845fe71ab2719a5
Viola <viola.cavallini@student.unife.it>
2020-08-20 15:30:16 +0200

    aggiunte le stampe

--- prefixScanWithoutCub/dpct_out/print_info.cpp
+++ prefixScanWithoutCub/dpct_out/print_info.cpp
@@ -5,25 +5,62 @@
 
 using namespace cms::cuda;
 
-void ker(sycl::nd_item<3> item_ct1, sycl::stream stream_ct1) {}
+void ker(sycl::nd_item<3> item_ct1, sycl::stream stream_ct1) {
+
+  stream_ct1 << "\nglobal range " << item.get_global_range();
+  stream_ct1 << "\nglobal id " << item.get_global_id();
+  stream_ct1 << "\nglobal linear id " << item.get_global_linear_id();
+  stream_ct1 << "\ngroup range " << item.get_group_range();
+  stream_ct1 << "\ngroup " << item.get_group().get_id();
+  stream_ct1 << "\ngroup linear id " << item.get_group_linear_id();
+  stream_ct1 << "\nlocal range " << item.get_local_range();
+  stream_ct1 << "\nlocal id " << item.get_local_id();
+  stream_ct1 << "\nlocal linear id " << item.get_local_linear_id();
+  stream_ct1 << "\nsubgroup group range "
+             << item.get_sub_group().get_group_range();
+  stream_ct1 << "\nsubgroup group id " << item.get_sub_group().get_group_id();
+  stream_ct1 << "\nsubgroup local range "
+             << item.get_sub_group().get_local_range();
+}
 
 int main() {
   dpct::device_ext &dev_ct1 = dpct::get_current_device();
   sycl::queue &q_ct1 = dev_ct1.default_queue();
 
-  // max work_item_sizes and max work group size
+  std::cout << "\ndevice type: "
+            << dev_ct1.get_info<sycl::info::device::device_type>() << std::endl;
+
+  std::cout << "\nmax number of parallel compute units: "
+            << dev_ct1.get_info<sycl::info::device::max_compute_units>()
+            << std::endl;
+
+  std::cout << "\nmax work item dimensions (max dim that specify the global "
+               "and local work-item IDs): "
+            << dev_ct1.get_info<sycl::info::device::max_work_item_dimensions>()
+            << std::endl;
+
   auto max_item_size =
       dev_ct1.get_info<sycl::info::device::max_work_item_sizes>();
-  std::cout << "\nmax item sizes: " << max_item_size[0] << ' '
-            << max_item_size[1] << ' ' << max_item_size[2];
+  std::cout << "\nmax item sizes (max number of work-items permitted in each "
+               "dimension of the work-group): "
+            << max_item_size[0] << ' ' << max_item_size[1] << ' '
+            << max_item_size[2];
 
   auto max_work_group_size =
       dev_ct1.get_info<sycl::info::device::max_work_group_size>();
-  std::cout << "\nmax work group sizes: " << max_work_group_size << std::endl;
+  std::cout << "\nmax work group size (max number of work-items permitted in a "
+               "work-group): "
+            << max_work_group_size << std::endl;
 
+  std::cout << "\nmax number of sub-groups in a work group: "
+            << dev_ct1.get_info<sycl::info::device::max_num_sub_groups>()
+            << std::endl;
+
+  std::cout << "\nsub-group sizes: ";
   auto dim_subgroup = dev_ct1.get_info<sycl::info::device::sub_group_sizes>();
-  std::cout << (int)dim_subgroup[0] << ' ' << (int)dim_subgroup[1] << ' '
-            << (int)dim_subgroup[2];
+  for (int const &el : dim_subgroup) {
+    std::cout << el << " ";
+  }
 
   // std::cout << "\nmax work item dimentions: ";
   // std::cout <<
@@ -41,7 +78,7 @@ int main() {
         co_acc_ct1(sycl::range<1>(1024), cgh);
 
     cgh.parallel_for(
-        sycl::nd_range<3>(sycl::range<3>(1, 1, 16), sycl::range<3>(1, 1, 16)),
+        sycl::nd_range<3>(sycl::range<3>(1, 1, 8), sycl::range<3>(1, 1, 8)),
         [=](sycl::nd_item<3> item_ct1) { ker<int>(item_ct1, stream_ct1); });
   });
   dev_ct1.queues_wait_and_throw();
