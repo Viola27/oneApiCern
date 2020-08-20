bd75fd41c4db2bfedebcd3fa48c6fe1712808fc1
Viola <viola.cavallini@student.unife.it>
2020-08-20 15:34:57 +0200

    sistemati errori di compilazione

--- prefixScanWithoutCub/dpct_out/print_info.cpp
+++ prefixScanWithoutCub/dpct_out/print_info.cpp
@@ -3,32 +3,32 @@
 #include <iostream>
 #include <stdlib.h>
 
-using namespace cms::cuda;
-
 void ker(sycl::nd_item<3> item_ct1, sycl::stream stream_ct1) {
 
-  stream_ct1 << "\nglobal range " << item.get_global_range();
-  stream_ct1 << "\nglobal id " << item.get_global_id();
-  stream_ct1 << "\nglobal linear id " << item.get_global_linear_id();
-  stream_ct1 << "\ngroup range " << item.get_group_range();
-  stream_ct1 << "\ngroup " << item.get_group().get_id();
-  stream_ct1 << "\ngroup linear id " << item.get_group_linear_id();
-  stream_ct1 << "\nlocal range " << item.get_local_range();
-  stream_ct1 << "\nlocal id " << item.get_local_id();
-  stream_ct1 << "\nlocal linear id " << item.get_local_linear_id();
+  stream_ct1 << "\nglobal range " << item_ct1.get_global_range();
+  stream_ct1 << "\nglobal id " << item_ct1.get_global_id();
+  stream_ct1 << "\nglobal linear id " << item_ct1.get_global_linear_id();
+  stream_ct1 << "\ngroup range " << item_ct1.get_group_range();
+  stream_ct1 << "\ngroup " << item_ct1.get_group().get_id();
+  stream_ct1 << "\ngroup linear id " << item_ct1.get_group_linear_id();
+  stream_ct1 << "\nlocal range " << item_ct1.get_local_range();
+  stream_ct1 << "\nlocal id " << item_ct1.get_local_id();
+  stream_ct1 << "\nlocal linear id " << item_ct1.get_local_linear_id();
   stream_ct1 << "\nsubgroup group range "
-             << item.get_sub_group().get_group_range();
-  stream_ct1 << "\nsubgroup group id " << item.get_sub_group().get_group_id();
+             << item_ct1.get_sub_group().get_group_range();
+  stream_ct1 << "\nsubgroup group id "
+             << item_ct1.get_sub_group().get_group_id();
   stream_ct1 << "\nsubgroup local range "
-             << item.get_sub_group().get_local_range();
+             << item_ct1.get_sub_group().get_local_range();
 }
 
 int main() {
   dpct::device_ext &dev_ct1 = dpct::get_current_device();
   sycl::queue &q_ct1 = dev_ct1.default_queue();
 
-  std::cout << "\ndevice type: "
-            << dev_ct1.get_info<sycl::info::device::device_type>() << std::endl;
+  /*std::cout << "\ndevice type: "
+            << dev_ct1.get_info<sycl::info::device::device_type>() <<
+     std::endl;*/
 
   std::cout << "\nmax number of parallel compute units: "
             << dev_ct1.get_info<sycl::info::device::max_compute_units>()
@@ -79,7 +79,7 @@ int main() {
 
     cgh.parallel_for(
         sycl::nd_range<3>(sycl::range<3>(1, 1, 8), sycl::range<3>(1, 1, 8)),
-        [=](sycl::nd_item<3> item_ct1) { ker<int>(item_ct1, stream_ct1); });
+        [=](sycl::nd_item<3> item_ct1) { ker(item_ct1, stream_ct1); });
   });
   dev_ct1.queues_wait_and_throw();
 }
